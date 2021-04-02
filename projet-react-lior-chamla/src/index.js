09f1685b2fc565304ec1e39ee3428b81b8f47a8a
marquillier <nicolas.marquillier@elosi.com>
2021-04-02 15:39:11 +0200

    41m30 - Liste des clients
    mise en place de la fonction de suppresion d'un element du tableau

--- projet-react-lior-chamla/src/index.js
+++ projet-react-lior-chamla/src/index.js
@@ -7,34 +7,53 @@ class App extends React.Component {
 
   state = {
     clients: [
-      {id: 1, nom: "Nicolas Marquillier"},
-      {id: 2, nom: "Caroline Marquillier"},
-      {id: 3, nom: "Ines Marquillier"},
-      {id: 4, nom: "Elise Marquillier"},
-      {id: 5, nom: "Hugo Marquillier"},
+      { id: 1, nom: "Nicolas Marquillier" },
+      { id: 2, nom: "Caroline Marquillier" },
+      { id: 3, nom: "Ines Marquillier" },
+      { id: 4, nom: "Elise Marquillier" },
+      { id: 5, nom: "Hugo Marquillier" }
     ]
+  };
+
+  handleDelete = (id) => {
+    // la methode slice permet de créer une copie d'un tableau on ne travail pas directement sur le tableau mais sur une copie
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(function (client) {
+      return client.id === id
+    });
+    // la methode plice permet de supprimer un element du tableau
+    clients.splice(index, 1);
+    // Le setstate permet de mettre à jour un élément
+    this.setState({ clients: clients });
   }
 
-handleClick() {
-  alert("Bonjour à vous");
-}
+  // delClick = () => {
+  //   // alert("Bonjour à vous");
+  //   //this.setState({compteur: this.state.compteur + 1});
+  //   //console.log(this.state);
+
+  //   // la methode slice permet de créer une copie d'un tableau
+  //   const clients = this.state.clients.shift();
+  //   clients.push({ id: 4, nom: "Nesquik"})
+  //   //le setState permet de changer l'etat d'un composant
+  //   this.setState({clients: clients});
+  // }
 
   render() {
     const title = "Liste des clients"
-    // const element = <li>test variable</li>
-    const elements = this.state.clients.map((client) =>
-      <li>{client.nom} <button>X</button></li>
-    )
-    
+
     return (
       <div>
         <h1>{title}</h1>
-        <button onClick={this.handleClick}>Click me</button>
         <ul>
-          {elements}
+          {this.state.clients.map(client => (
+            <li>
+              {client.nom} <button onClick={() => this.handleDelete(client.id)}>X</button>
+            </li>
+          ))}
         </ul>
         <form>
-          <input type="text" placeholder="Ajouter un client"/>
+          <input type="text" placeholder="Ajouter un client" />
           <button>Confirmer</button>
         </form>
       </div>
